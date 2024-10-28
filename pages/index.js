5969d637219ff64c6661b885e86f9ac47e81e75a
sachin798815 <sachin798815@gmail.com>
2024-10-28 16:59:17 +0530

    Main index created

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,10 @@
-function TodoHomePage(){
-  return <h1>The TodoHomePage</h1>
+import { Container } from "react-bootstrap";
+
+function TodoHomePage() {
+  return (
+    <Container className="border border-warning shadow p-3 mb-5 bg-white rounded">
+      <h1>The TodoHomePage</h1>
+    </Container>
+  );
 }
 export default TodoHomePage;
