eed6d82bb0761602731d08397ed2222febfa2fa9
sachin798815 <sachin798815@gmail.com>
2024-10-29 19:13:32 +0530

    Nav links added, Nextjs installed

--- Components/Header/NavComponent.js
+++ Components/Header/NavComponent.js
@@ -1,17 +1,18 @@
+import Link from "next/link";
 import { Navbar } from "react-bootstrap";
 
 const NavComponent = () => {
   return (
     <>
-      <Navbar expand="lg" className="bg-body-tertiary">
+      <Navbar expand="lg" className="bg-body-tertiary warning">
         <Container>
           <Navbar.Brand href="#home">Todo List</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Todos</Nav.Link>
-              <Nav.Link href="#link">Completed Todos</Nav.Link>
+              <Nav.Link href="#home"><Link>Home</Link></Nav.Link>
+              <Nav.Link href="#link"><Link>Todos</Link></Nav.Link>
+              <Nav.Link href="#link"><Link>Completed Todos</Link></Nav.Link>
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">About</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Contacts</NavDropdown.Item>
