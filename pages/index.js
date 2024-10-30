42bb494e18305441e90cea02bf4e85c1414c6800
sachin798815 <sachin798815@gmail.com>
2024-10-30 22:16:24 +0530

    pages updated

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import { Container } from "react-bootstrap";
+import TodoHomePage from '../Components/Body/TodoMainPage';
 
 function TodoHomePage() {
   return (
     <Container className="border border-warning shadow p-3 mb-5 bg-white rounded">
       <h1>The TodoHomePage</h1>
+      <TodoMainPage/>
     </Container>
   );
 }
