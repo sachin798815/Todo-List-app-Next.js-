b0f3505b381a6c5edc01513dc8b7861b75348fe5
sachin798815 <sachin798815@gmail.com>
2024-10-31 12:43:12 +0530

    Head part added

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,24 @@
 import { Container } from "react-bootstrap";
 import TodoHomePage from '../Components/Body/TodoMainPage';
 import NavComponent from '../Components/Header/NavComponent';
+import Head from "next/head";
 
 function TodoHomePage() {
-  return (
+  return (<>
+  <Head>
+      <title>Meetups</title>
+      <meta
+      name="description"
+      content="Best meetups in your city"
+      >
+      </meta>
+    </Head>
     <Container className="border border-warning shadow p-3 mb-5 bg-white rounded">
       <NavComponent/>
       <h1>The TodoHomePage</h1>
       <TodoMainPage/>
     </Container>
+  </>
   );
 }
 export default TodoHomePage;
