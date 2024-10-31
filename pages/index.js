e5e0937dabe9db071f42abdad9f1453d54d838f2
sachin798815 <sachin798815@gmail.com>
2024-10-31 12:19:07 +0530

    Nav added to main

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import { Container } from "react-bootstrap";
 import TodoHomePage from '../Components/Body/TodoMainPage';
+import NavComponent from '../Components/Header/NavComponent';
 
 function TodoHomePage() {
   return (
     <Container className="border border-warning shadow p-3 mb-5 bg-white rounded">
+      <NavComponent/>
       <h1>The TodoHomePage</h1>
       <TodoMainPage/>
     </Container>
