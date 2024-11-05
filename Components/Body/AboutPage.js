02d4a187fef0b8a2d3dc25da7aa8e846f065cb45
sachin798815 <sachin798815@gmail.com>
2024-11-05 21:05:08 +0530

    about page added

---
+++ Components/Body/AboutPage.js
@@ -0,0 +1,15 @@
+import { Container } from "react-bootstrap";
+
+const AboutPage=()=>{
+    return(
+        <>
+        <Container>
+            <h1>The about page</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam iure neque veritatis alias animi sequi autem quasi exercitationem quo iusto suscipit consectetur repellat nisi expedita incidunt ullam, aut quisquam ea.</p>
+            
+        </Container>
+        </>
+    )
+}
+
+export default AboutPage;
