d74a6f6e22f1e7198e075d6712ce32275eab4e9e
sachin798815 <sachin798815@gmail.com>
2024-11-06 21:57:55 +0530

    contact us added

---
+++ Components/Body/ContactPage.js
@@ -0,0 +1,15 @@
+import { Container } from "react-bootstrap";
+
+const ContactPage=()=>{
+    return(
+        <>
+        <Container>
+            <h1>The contact page</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam iure neque veritatis alias animi sequi autem quasi exercitationem quo iusto suscipit consectetur repellat nisi expedita incidunt ullam, aut quisquam ea.</p>
+            
+        </Container>
+        </>
+    )
+}
+
+export default ContactPage;
