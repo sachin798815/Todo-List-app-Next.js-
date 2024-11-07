9ee218671b9a7d6375040c0277d30b6ccb2d6f4e
sachin798815 <sachin798815@gmail.com>
2024-11-07 15:11:47 +0530

    main index fixed

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import { Container } from "react-bootstrap";
 import TodoHomePage from '../Components/Body/TodoMainPage';
-import NavComponent from '../Components/Header/NavComponent';
 import Head from "next/head";
 import { MongoClient } from "mongodb";
 
@@ -16,7 +15,6 @@ function TodoHomePage() {
       </meta>
     </Head>
     <Container className="border border-warning shadow p-3 mb-5 bg-white rounded">
-      <NavComponent/>
       <h1>The TodoHomePage</h1>
       <TodoMainPage/>
     </Container>
