246b07268cf2a8cb3ededad1b36d3dfbb5206f31
sachin798815 <sachin798815@gmail.com>
2024-12-13 11:50:35 +0530

    review page added

---
+++ Components/Body/ReviewPage.js
@@ -0,0 +1,24 @@
+import { Form } from "react-bootstrap";
+
+const ReviewPage=()=>{
+    return(
+        <>
+        <h1>Reviews</h1>
+        <p>Please write your reviews here</p>
+        <Form>
+        <Form.Label>Add todo here</Form.Label>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Review Title</Form.Label>
+            <Form.Control type="text" placeholder="Enter title" ref={titleRef}/>
+            <Form.Label>Review Description</Form.Label>
+            <Form.Control type="text" placeholder="Enter detailed description" ref={descriptionRef}/>
+          </Form.Group>
+          <Button type="submit" variant="warning" onClick={addReviewHandler}>
+            ADD
+          </Button>
+        </Form>
+        </>
+    )
+}
+
+export default ReviewPage;
