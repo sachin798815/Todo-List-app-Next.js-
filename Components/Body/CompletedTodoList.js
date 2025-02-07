0bdae70df6d4d71aace0900bc2bc1f9b18bd702c
sachin798815 <sachin798815@gmail.com>
2025-02-07 16:07:21 +0530

    completed todos aligned correctly

--- Components/Body/CompletedTodoList.js
+++ Components/Body/CompletedTodoList.js
@@ -1,48 +1,51 @@
 import { Button, Container } from "react-bootstrap";
-import styles from './CompletedTodoList.module.css';  // Importing CSS module
+import styles from './CompletedTodoList.module.css';
+import { useState } from "react";
 
 const CompletedTodoList = (props) => {
-  
-  const deleteButtonHandler = async (todo) => {
-    try {
-      const response = await fetch("/api/delete-todo", {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ id: todo.id }),
-      });
-      const data = await response.json();
-      console.log(data.message);
-      window.location.reload();
-    } catch (error) {
-      console.error("Error deleting todo:", error);
-    }
-  };
-  
-  return (
-    <Container className={styles.container}>
-      <ul className={styles.todoList}>
-        {props.TodoList.map((todo) =>
-          todo.status !== "pending" ? (
-            <li key={todo.id} className={styles.todoListItem}>
-              <div className={styles.todoContent}>
-                <span className={styles.todoTitle}>{todo.title}</span>{todo.description}
-                <div className={styles.todoButtons}>
-                  <Button
-                    className={styles.deleteButton}
-                    onClick={() => deleteButtonHandler(todo)}
-                  >
-                    Delete
-                  </Button>
-                </div>
-              </div>
-            </li>
-          ) : null
-        )}
-      </ul>
-    </Container>
-  );
+    const [todosArray, setTodosArray] = useState(props.TodoList);
+
+    const deleteButtonHandler = async (todo) => {
+        try {
+            const response = await fetch("/api/delete-todo", {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ id: todo.id }),
+            });
+            const data = await response.json();
+            console.log(data.message);
+        } catch (error) {
+            console.error("Error deleting todo:", error);
+        }
+        setTodosArray(todosArray.filter(todos => todos.id !== todo.id));
+    };
+
+    return (
+        <Container className={styles.container}>
+            <ul className={styles.todoList}>
+                {todosArray.map((todo) =>
+                    todo.status !== "pending" ? (
+                        <li key={todo.id} className={styles.todoListItem}>
+                            <div className={styles.todoContent}>
+                                <span className={styles.todoTitle}>{todo.title}</span>
+                                {todo.description}
+                            </div>
+                            <div className={styles.todoButtons}>
+                                <Button
+                                    className={styles.deleteButton}
+                                    onClick={() => deleteButtonHandler(todo)}
+                                >
+                                    Delete
+                                </Button>
+                            </div>
+                        </li>
+                    ) : null
+                )}
+            </ul>
+        </Container>
+    );
 };
 
 export default CompletedTodoList;
